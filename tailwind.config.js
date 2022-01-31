a7657534bb2342743708066963b5e581fb028541
Oleh Pratsko <olehpratsko@gmail.com>
2022-01-31 22:00:44 +0200

    chore(config): cleanup config and fix deprecated issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,24 @@
 /* eslint-disable no-multi-spaces, key-spacing */
 
 module.exports = {
-    purge: [
+    content: [
         'src/**/*.jsx',
     ],
-    darkMode: false, // or 'media' or 'class'
     theme: {
         fontFamily: {
             display: ['Open Sans', 'Helvetica', 'sans-serif'],
             body: ['Open Sans', 'Helvetica', 'sans-serif'],
         },
         fontSize: {
-            xs: ['0.75rem',   '1.5rem'],
-            sm: ['0.875rem',  '1.5rem'],
-            base: ['1rem',      '2rem'],
-            intro: ['1.0625rem', '2rem'],
-            lg: ['1.125rem',  '1.75rem'],
-            xl: ['1.25rem',   '1.75rem'],
-            '2xl': ['1.5rem',    '2rem'],
-            '3xl': ['1.875rem',  '2.5rem'],
-            '4xl': ['2.25rem',   '3rem'],
-            '5xl': ['3rem',      '3.5rem'],
-            '6xl': ['3.75rem',   '1'],
-            '7xl': ['4.5rem',    '1'],
-            '8xl': ['5.5rem',    '1'],
-            '9xl': ['7.5rem',    '1'],
+            xs: ['0.75rem',      '1.5'],
+            sm: ['0.875rem',     '1.5'],
+            base: ['1rem',       '1.5'],
+            lg: ['1.125rem',     '1.75'],
+            xl: ['1.25rem',      '1.75'],
+            '2xl': ['1.5rem',    '2'],
+            '3xl': ['1.875rem',  '2.5'],
+            '4xl': ['2.25rem',   '3'],
+            '5xl': ['3rem',      '3.5'],
         },
         extend: {
             colors: {
